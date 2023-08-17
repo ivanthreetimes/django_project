afaff457f88f2c83b1df3f47bb2c959962dd1f69
Ivan Ivanov <ivanthreetimes@gmail.com>
2023-08-17 17:16:56 +0300

    Update profile system added

--- django_project/app_users/views.py
+++ django_project/app_users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import AppUserRegisterForm
+from .forms import AppUserRegisterForm, AppUserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -23,4 +23,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'app_users/profile.html')
+    if request.method == 'POST':
+        u_form = AppUserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = AppUserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'app_users/profile.html', context)
